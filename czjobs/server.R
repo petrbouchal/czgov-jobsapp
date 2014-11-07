f06022f452f422b55fde492ded2b24e14e00ebfd
Petr Bouchal <pbouchal@gmail.com>
2014-11-07 00:39:10 +0000

    running with firsteen-lastseen data model

--- czjobs/server.R
+++ czjobs/server.R
@@ -1,15 +1,17 @@
-## Query: select '<a href=''' || joburl || ''' target=''_blank''>' || jobtitle || '</a>' as Pozice, dept as Ministerstvo from 'data' where datetime = (select max(datetime) from data) order by Ministerstvo
-## Date query: select max(datetime) as date from data
+## Query: select '<a href=''' || joburl || ''' target=''_blank''>' || jobtitle || '</a>' as Pozice, dept as Organizace, seenfor, live, latest from 'data' where live = 1 order by Organizace
+## Query w/out <a> etc: select joburl,jobtitle, dept as Organizace, seenfor, live, latest from 'data' where live = 1 order by Organizace
+## Date query: select max(lastseen) as date from data
 
+library(plyr)
+library(dplyr)
 library(rCharts)
 library(RCurl)
 library(jsonlite)
-library(dplyr)
 # library(googleVis)
 
-url_data <- 'https://api.morph.io/petrbouchal/GovJobsCZ/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20%27%3Ca%20href%3D%27%27%27%20%7C%7C%20joburl%20%7C%7C%20%27%27%27%20target%3D%27%27_blank%27%27%3E%27%20%7C%7C%20jobtitle%20%7C%7C%20%27%3C%2Fa%3E%27%20as%20Pozice%2C%20dept%20as%20Organizace%20from%20%27data%27%20where%20datetime%20%3D%20(select%20max(datetime)%20from%20data)%20order%20by%20Organizace%20'
-url_date <- 'https://api.morph.io/petrbouchal/GovJobsCZ/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20max(datetime)%20as%20date%20from%20data'
-url_allorgs <- 'https://api.morph.io/petrbouchal/GovJobsCZ/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20count(distinct(dept))%20as%20alldeptcount%20from%20%27data%27'
+url_data <- 'https://api.morph.io/petrbouchal/czgov-jobs-test/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20joburl%2Cjobtitle%2C%20dept%20as%20Organizace%2C%20seenfor%2C%20live%2C%20latest%20from%20%27data%27%20where%20live%20%3D%201%20order%20by%20seenfor'
+url_date <- 'https://api.morph.io/petrbouchal/czgov-jobs-test/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20max(lastseen)%20as%20date%20from%20data'
+url_allorgs <- 'https://api.morph.io/petrbouchal/czgov-jobs-test/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20count(distinct(dept))%20as%20alldeptcount%20from%20%27data%27'
 tmpFile <- tempfile()
 tmpFile <- getURL(url_data,.opts = list(ssl.verifypeer = FALSE))
 data <- fromJSON(tmpFile)
@@ -22,26 +24,14 @@ alldeptcount <- fromJSON(tmpFile3)[1,1]
 datum <- strptime(date$date, '%Y-%m-%d')
 datum <- strftime(date$date, '%d.%m.%Y')
 deptcount <- length(unique(data$Organizace))
-jobcount <- length(unique(data$Pozice))
+jobcount <- length(unique(data$joburl))
 
 load('./names.dta')
 data <- merge(data,names,all.x=TRUE)
-
-gcoptions = list(width = '100%',
-                 height = 320,
-                 bar = '{groupWidth: \"85%\"}',
-                 colors="['red']",
-                 legend = list(position = "none"),
-                 titlePosition = 'none',
-                 chartArea = '{left: 60, top: 25, height: \'85%\'}',
-                 axisTitlesPosition = 'none',
-                 theme = 'maximized',
-                 vAxis = '{textStyle: {color: \'red\', bold: false,
-                           fontFace: \'Source Sans Pro\'},
-                           textPosition: \'out\'}')
+data$Pozice <- paste0('<a href=\"',data$joburl,' target=\"_blank\">',data$jobtitle,'</a>')
 
 data2 <- data %>% group_by(zkratka) %>% summarise(pozic=n()) %>% arrange(-pozic)
-tabledata <- select(data, Pozice, Organizace=fullnazev)
+tabledata <- select(data, Pozice, Organizace=fullnazev, seenfor) %>% arrange(seenfor)
 
 shinyServer(function(input, output) {
   output$counttext <- renderText(paste0(' Nalezeno ',jobcount,' nabídek od ',
@@ -60,12 +50,13 @@ shinyServer(function(input, output) {
                                                      pageLength=10,
                                                      dom="<<t>pi>",
                                                      searching=T,
-                                                     columns=list(list('width'='75%', 'title'='Pozice'),
-                                                                  list('width'='25%', 'title'='Organizace'))))
-#   output$googlechart <- renderGvis((gvisBarChart(data2,'zkratka','pozic',
-#                                                  options=gcoptions)))
+                                                     columns=list(list('width'='70%', 'title'='Pozice'),
+                                                                  list('width'='20%', 'title'='Organizace'),
+                                                                  list('width'='10%', 'title'='Stáří (dny)'))))
+
   output$rchart <- renderChart2({
     rch <- uPlot('zkratka','pozic', data=data2, type='StackedBar')
+#     rch <- uPlot('zkratka','pozic', data=data2, type='StackedBar', group='seenfor')
     rch$params$width <- 800
     rch$params$height <- 400
     rch$params$margin <- 0
