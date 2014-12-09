2e5fccca571bc0dcb6b180bb87dc1991e511b6cb
Petr Bouchal <pbouchal@gmail.com>
2014-12-09 23:30:07 +0000

    clean up UI script

--- czjobs/ui.R
+++ czjobs/ui.R
@@ -1,12 +1,12 @@
 library(ggvis)
 shinyUI(fluidPage(theme='custom.css',
   tags$head(
-    includeScript('analytics-code-test.js'),
-    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
+    includeScript('analytics-code-test.js')),
+#     tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
   fluidRow(
-    textOutput('counttext'),
-    tags$div(style="max-width:800px; height:400px;",
-             ggvisOutput('graf')),
+    textOutput('counttext')),
+  fluidRow(
+    ggvisOutput('graf'),
     dataTableOutput('data'),
     tags$style(type="text/css", '#data tfoot {display: table-header-group;}'),
     tags$br(),
