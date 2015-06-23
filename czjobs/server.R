1275f5c51218de39700e55376481c371da9e8dcb
petrbouchal <pbouchal@gmail.com>
2015-06-24 00:01:56 +0200

    basic filtering by clicking on image

--- czjobs/server.R
+++ czjobs/server.R
@@ -3,10 +3,11 @@
 
 library(plyr)
 library(dplyr)
-library(rCharts)
+library(ggplot2)
 library(RCurl)
 library(httr)
 library(jsonlite)
+library(DT)
 # library(googleVis)
 
 url_data <- 'https://api.morph.io/petrbouchal/GovJobsCZ/data.json?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20joburl%2C%20jobtitle%2C%20dept%20as%20Organizace%20from%20%27data%27%20where%20datetime%20%3D%20(select%20max(datetime)%20from%20data)%20order%20by%20Organizace%20'
@@ -31,8 +32,10 @@ load('./names.dta')
 data <- merge(data,names,all.x=TRUE)
 data$Pozice <- paste0('<a href=\"',data$joburl,'\" target=\"_blank\">',data$jobtitle,'</a>')
 
-data2 <- data %>% group_by(zkratka) %>% summarise(pozic=n()) %>% arrange(-pozic)
-tabledata <- select(data, Pozice, Organizace=fullnazev)
+data2 <- data %>% group_by(zkratka) %>% summarise(pozic=n()) %>% arrange(zkratka)
+data2$zkratka <- droplevels(data2$zkratka)
+tabledata <- select(data, Pozice, Organizace=fullnazev, zkratka) %>% arrange(zkratka)
+tabledata$zkratka <- droplevels(tabledata$zkratka)
 jobcount <- nrow(data)
 
 shinyServer(function(input, output) {
@@ -41,36 +44,45 @@ shinyServer(function(input, output) {
                                         ' organizací. Naposledy zkontrolováno ',
                                         datum,'. Sledujeme ', alldeptcount,
                                         ' organizací.'))
-  output$data <- DT::renderDataTable(tabledata, filter='top', rownames=FALSE,
-                                 options = list(lengthChange=F,
-                                                language=list(
-                                                  "paginate"=list("next"="další",
-                                                                  "previous"="předchozí"),
-                                                  "infoEmpty"="Nic nenalezeno",
-                                                  "zeroRecords"="Hledání neodpovídá žádná pozice",
-                                                  "info"="Pozice _START_ až _END_ z _TOTAL_",
-                                                  "infoFiltered"=" (celkem _MAX_)"),
-                                                pageLength=10,
-                                                # pagingType="simple_num",
-                                                dom="<<t>pi>",
-                                                searching=T,
-                                                columns=list(list("type"="html","width"="75%", "title"="Pozice"),
-                                                             list("width"="25%", "title"="Organizace"))
-                                                ),
-                                 escape=FALSE)
-  output$rchart <- renderChart2({
+  output$data <- DT::renderDataTable(
+    if(!is.null(input$plot_brush)) {
+      tabledata[tabledata$zkratka %in% (levels(data2$zkratka)[seq(round(input$plot_brush$ymin),
+                                                                  round(input$plot_brush$ymax))]), c(1,2)]
+    } else {
+      if (!is.null(input$plot_click)) {
+        tabledata[tabledata$zkratka==levels(data2$zkratka)[round(input$plot_click$y)],c(1:2)]
+      } else {
+        tabledata[,c(1,2)]
+      }
+    }
+    ,filter='top', rownames=FALSE,
+    options = list(lengthChange=F,
+                   language=list(
+                     "paginate"=list("next"="další",
+                                     "previous"="předchozí"),
+                     "infoEmpty"="Nic nenalezeno",
+                     "zeroRecords"="Hledání neodpovídá žádná pozice",
+                     "info"="Pozice _START_ až _END_ z _TOTAL_",
+                     "infoFiltered"=" (celkem _MAX_)"),
+                   pageLength=10,
+                   # pagingType="simple_num",
+                   dom="<<t>pi>",
+                   searching=T,
+                   columns=list(list("type"="html","width"="70%", "title"="Pozice"),
+                                list("width"="20%", "title"="Organizace"))
+    ),
+    escape=FALSE)
+  output$chart <- renderPlot({
     # This is a workaround to keep the ordering
     # as per https://github.com/ramnathv/rCharts/issues/212
 #     rch <- hPlot(x='zkratka',y='pozic', data=data2, type='bar')
-    rch <- Highcharts$new()
-    rch$params$width <- '100%'
-    rch$params$height <- 400
-    rch$series(data = data2$pozic, type = "bar", pointWidth=400/deptcount-6,name='Nabídek')
-    rch$plotOptions(bar = list(stacking = "normal", borderColor=NA))
-    rch$xAxis(tickLength=0,type='category', categories=data2$zkratka)
-    rch$yAxis(tickLength=0,title=NA,
-              tickPositions=seq(0,ceiling(max(data2$pozic)/10)*10,10))
-    rch$legend(enabled=FALSE)
+    rch <- ggplot(data2, aes(zkratka, pozic)) +
+      geom_bar(stat="identity", fill="#4AA1F1") +
+      coord_flip() +
+      scale_y_continuous(expand=c(0,0)) +
+      theme_minimal() + theme(axis.ticks=element_blank(),
+                              axis.title=element_blank(),
+                              panel.grid.major.y=element_blank())
     return(rch)
   })
 })
--- czjobs/ui.R
+++ czjobs/ui.R
@@ -1,5 +1,4 @@
-require(rCharts)
-# options(RCHART_LIB = 'uvcharts')
+require(ggplot2)
 shinyUI(fluidPage(
     includeCSS('./www/custom.css'),
     tags$head(
@@ -7,12 +6,24 @@ shinyUI(fluidPage(
   fluidRow(
     textOutput('counttext'),
 #     htmlOutput('googlechart'),
-    showOutput('rchart','highcharts'),
+    column(width=4,  
+      plotOutput('chart', 
+                 click = "plot_click",
+                 dblclick = dblclickOpts(
+                   id = "plot_dblclick"
+                 ),
+                 hover = hoverOpts(
+                   id = "plot_hover"
+                 ),
+                 brush = brushOpts(
+                   id = "plot_brush",
+                   direction = "y"))),
+  column(width=8,
     DT::dataTableOutput('data'),
     tags$style(type="text/css", '#data tfoot {display: table-header-group;}'),
     tags$br(),
     tags$span(tags$a(href='http://www.morph.io/petrbouchal/GovJobsCZ','Data'),
               ' | ',
               tags$a(href='http://www.github.com/petrbouchal/GovJobsCZ','Kód'))
-  )
+  ))
 ))
