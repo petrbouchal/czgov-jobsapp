738c9d0e2809dafc3bc3d47f65cf785b438a0895
petrbouchal <pbouchal@gmail.com>
2016-06-30 21:53:47 +0200

    fix chart ticks

--- czjobs/server.R
+++ czjobs/server.R
@@ -68,8 +68,9 @@ shinyServer(function(input, output) {
     rch$series(data = data2$pozic, type = "bar", pointWidth=400/deptcount-6,name='Nabídek')
     rch$plotOptions(bar = list(stacking = "normal", borderColor=NA))
     rch$xAxis(tickLength=0,type='category', categories=data2$zkratka)
-    rch$yAxis(tickLength=0,title=NA,
-              tickPositions=seq(0,ceiling(max(data2$pozic)/10)*10,10))
+    rch$yAxis(tickLength=0,
+              # tickPositions=seq(0,ceiling(max(data2$pozic)/10)*10,10),
+              title=NA)
     rch$legend(enabled=FALSE)
     return(rch)
   })
